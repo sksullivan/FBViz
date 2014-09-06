68e80d3e89df34a048ffa11a19a6266854d37d44
Allen Kleiner <akleiner24@gmail.com>
2014-09-06 00:15:59 -0500

    gulpfile and stuffs

---
+++ Gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'); 
+
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+	return gulp.src('public/sass/*.scss')
+				.pipe(sass())
+				.pipe(gulp.dest('public/stylesheets'));
+}); 
+
+gulp.task('lint', function(){
+	return gulp.src('public/js/*.js')
+				.pipe(jshint())
+				.pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('public/js/*.js', ['lint']);
+	gulp.watch('public/sass/*.scss', ['sass']);
+});
+
+gulp.task('default', ['watch']);
