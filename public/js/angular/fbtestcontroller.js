b36ef79af4bc83c1538a55e3d86b731b798c54bc
Stephen Sullivan <sullstecclub@gmail.com>
2014-09-06 11:41:55 -0400

    Adding dynamic pathing support (not finished)

--- public/js/angular/fbtestcontroller.js
+++ public/js/angular/fbtestcontroller.js
@@ -1,16 +1,16 @@
-var fbVizApp = angular.module('fbVizApp',[]);
+var fbVizApp = angular.module('fbVizApp',['uiSlider']);
 
-fbVizApp.controller('fbtestcontroller', function ($scope) {
+fbVizApp.controller('fbtestcontroller', function ($scope, $http) {
 	$scope.init = function () {
 		FB.init({
 			appId: '166064833600885'
 		});
 		$scope.posts = [{story:"Click above..."}];
 		L.mapbox.accessToken = 'pk.eyJ1IjoiYndhbmcxOSIsImEiOiIyMXFLVW1VIn0.qrzAS2lCItHR6CaqxqY3pA';
-		var map = L.mapbox.map('map', 'bwang19.je7fg9i6');
-		var runLayer = omnivore.kml('/js/history.kml').on('ready', function() {
-        	map.fitBounds(runLayer.getBounds());
-    	}).addTo(map);
+		$scope.map = L.mapbox.map('map', 'bwang19.je7fg9i6');
+		// $scope.runLayer = omnivore.kml('/js/history.kml');
+    	$scope.rangeMin = 0;
+    	$scope.rangeMax = 100;
 	};
 	$scope.login = function () {
 		FB.getLoginStatus(function (response) {
@@ -44,6 +44,45 @@ fbVizApp.controller('fbtestcontroller', function ($scope) {
 					console.log('FB LOGIN ERROR: User cancelled login or did not fully authorize.');
 				}
 			}, { scope: '' }); // The specific permissions we need from FB
+		});
+	}
+	$scope.$watch('rangeMin',function() {
+
+		//console.log($scope.runLayer);
+		//console.log($scope.runLayer._layers['28']._latlngs[0]);
+	});
+	$scope.test = function () {
+		var x2js = new X2JS();
+		if (x2js == undefined) {
+			return;
+		}
+		$http.get('/js/history.kml').success(function (data) {
+			$scope.kmlJSON = x2js.xml_str2json(data);
+
+			var coordList = [];
+			var dataPoints = 100;
+			for (var i=0;i<dataPoints;i++) {
+				var coord = $scope.kmlJSON.kml.Document.Placemark.Track.coord[i].__text.split(' ')
+				coordList.push([parseFloat(coord[1]), parseFloat(coord[0])]);
+			}
+
+			var myLines = [{
+				"type": "LineString",
+				"coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+			}, {
+				"type": "LineString",
+				"coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+			}];
+
+			var myStyle = {
+				"color": "#ff7800",
+				"weight": 5,
+				"opacity": 0.65
+			};
+
+			L.geoJson(myLines, {
+				style: myStyle
+			}).addTo($scope.map);
 		});
 	}
     $scope.init();
